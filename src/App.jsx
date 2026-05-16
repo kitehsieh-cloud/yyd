5daadbe6ac30f48d6263f4f511dea35a51ce9ced
Kite <sp.KiteHsieh@gmail.com>
2026-05-16 14:03:55 +0800

    Refine companion animations

--- src/App.jsx
+++ src/App.jsx
@@ -722,7 +722,7 @@ function MainHeader({ photosByDay }) {
       <img src={DEFAULT_COMMON_HEADER_BG} alt="2026 東京自由行" />
       {[...new Set(companionIds)].map((dayNo) => {
         const pos = HEADER_COMPANION_LAYOUT[dayNo];
-        return pos ? <img key={dayNo} className={`heroCompanion heroCompanion-${dayNo}`} src={partnerImageUrl(dayNo)} alt={pos.label} style={{ left: `${pos.left}%`, top: `${pos.top}%`, width: `${pos.width}%` }} /> : null;
+        return pos ? <span key={dayNo} className={`heroCompanion heroCompanion-${dayNo}`} style={{ left: `${pos.left}%`, top: `${pos.top}%`, width: `${pos.width}%` }}><img src={partnerImageUrl(dayNo)} alt={pos.label} /></span> : null;
       })}
     </div>
   </header>;
